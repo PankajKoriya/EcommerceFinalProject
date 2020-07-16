381c3c6bfb87ff85c38f77a336adc43ec5b35ee0
pankaj.koriya <p.koriya1991@gmail.com>
2020-07-16 23:29:14 +0530

    Remove unwanted import statement

--- Ecommerce/src/test/java/com/ecommerce/pages/BaseClass.java
+++ Ecommerce/src/test/java/com/ecommerce/pages/BaseClass.java
@@ -2,14 +2,12 @@ package com.ecommerce.pages;
 
 import java.io.File;
 import java.io.IOException;
-
 import org.openqa.selenium.WebDriver;
 import org.testng.ITestResult;
 import org.testng.Reporter;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
