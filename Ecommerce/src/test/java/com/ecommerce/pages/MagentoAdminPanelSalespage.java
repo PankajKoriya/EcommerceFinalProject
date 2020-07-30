9aabc73bc3d3d5d061f1eb5c93fbe066b823aead
pankaj.koriya <p.koriya1991@gmail.com>
2020-07-30 17:05:56 +0530

    TestNg file changed

--- Ecommerce/src/test/java/com/ecommerce/pages/MagentoAdminPanelSalespage.java
+++ Ecommerce/src/test/java/com/ecommerce/pages/MagentoAdminPanelSalespage.java
@@ -3,11 +3,9 @@ package com.ecommerce.pages;
 import java.io.File;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.ui.Select;
-import org.testng.Assert;
 
 public class MagentoAdminPanelSalespage {
 	
